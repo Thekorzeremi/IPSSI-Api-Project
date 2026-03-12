9d5e03fcd7687e783b7d8694890412bce1d20092
WilFriite <jnguessan2003@gmail.com>
2026-03-12 12:54:31 +0100

    feat: configure Shadcn

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,14 +1,12 @@
+import { Button } from "@/components/ui/button";
 import { useState } from "react";
 
 export function HomePage() {
 	const [count, setCount] = useState(0);
 
 	return (
-		<>
-			<h1 class="text-3xl font-bold underline">Hello world!</h1>
-			<button type="button" onClick={() => setCount((count) => count + 1)}>
-				count is {count}
-			</button>
-		</>
+		<Button onClick={() => setCount((count) => count + 1)}>
+			count is {count}
+		</Button>
 	);
 }
